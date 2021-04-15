e3ed91e4eaec599487c987c3bd1fa136cba72c13
NevolinPetr <qazplmwo@yandex.ru>
2021-04-15 17:28:34 +0400

    добавление книги

--- forms/book.py
+++ forms/book.py
@@ -1,8 +1,15 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, SubmitField, TextAreaField
-from wtforms.fields.html5 import EmailField
+from flask_wtf.file import FileField
+from wtforms import DateField, StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 
 
 class BookForm(FlaskForm):
-    pass
+    title = StringField('Название', validators=[DataRequired()])
+    author = StringField('Автор', validators=[DataRequired()])
+    genre = StringField('Жанр', validators=[DataRequired()])
+    created_date = DateField('Дата создания', validators=[DataRequired()])
+    annotation = TextAreaField('Аннотация', validators=[DataRequired()])
+    img_file = FileField('Обложка', validators=[DataRequired()])
+    text_file = FileField('PDF файд', validators=[DataRequired()])
+    submit = SubmitField('Применить')
